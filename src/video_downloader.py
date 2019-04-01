8ee8811573b941e66e281401de2f5225d7dcfd68
Jacopo <bonta.jacopo@gmail.com>
2019-04-01 22:48:37 +0200

    spaces

--- src/video_downloader.py
+++ src/video_downloader.py
@@ -2,11 +2,11 @@ import pytube
 import threading
 
 def _download_and_save_video(url, path, convert):
-  yt  =   pytube.YouTube(url)
+  yt      =   pytube.YouTube(url)
   videos  =   yt.streams.first()
   videos.download(path)
 
 def download_and_save_video(url, path, convert):
-  thread  = threading.Thread(target=_download_and_save_video, args=(url, path, convert))
+  thread        = threading.Thread(target=_download_and_save_video, args=(url, path, convert))
   thread.daemon = True
   thread.start()
