ef3ba09ea963b05f8b7ce94748500c4f4e679c60
Jacopo <bonta.jacopo@gmail.com>
2019-04-01 23:17:41 +0200

    indent

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ from src.routes import download
 from src.errors import internal_error, invalid_parameters, not_found
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+  app.run(debug=True, host='0.0.0.0')
