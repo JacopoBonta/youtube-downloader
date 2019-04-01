8b4fa4d631ca47cb092609cb031d0dca376a0689
Jacopo <bonta.jacopo@gmail.com>
2019-04-01 22:45:40 +0200

    download video in bg

--- src/video_downloader.py
+++ src/video_downloader.py
@@ -1,8 +1,12 @@
 import pytube
+import threading
+
+def _download_and_save_video(url, path, convert):
+  yt  =   pytube.YouTube(url)
+  videos  =   yt.streams.first()
+  videos.download(path)
 
 def download_and_save_video(url, path, convert):
-    yt  =   pytube.YouTube(url)
-
-    videos  =   yt.streams.first()
-
-    videos.download(path)
+  thread  = threading.Thread(target=_download_and_save_video, args=(url, path, convert))
+  thread.daemon = True
+  thread.start()
