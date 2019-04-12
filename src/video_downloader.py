dacf6bcc82aa7cc9025a1b08181aaab5b19e34f6
Jacopo <bonta.jacopo@gmail.com>
2019-04-13 01:27:26 +0200

    add logs

--- src/video_downloader.py
+++ src/video_downloader.py
@@ -10,20 +10,19 @@ def _download_and_save_video(url, path, convert):
   download_id = store.add_download(url)
 
   try:
-
+    print('download started')
     yt      =   pytube.YouTube(url)
     videos  =   yt.streams.first()
     videos.download(path)
-
   except Exception as e:
-
+    print('download errored')
     print(e)
 
     store.set_status(download_id, 'DOWNLOAD_ERROR')
     store.log('ERROR', str(e))
 
   else:
-
+    print('download ended')
     store.set_status(download_id, 'READY')
     store.log('INFO', str(url) + ' download completato')
 
@@ -31,6 +30,7 @@ def _download_and_save_video(url, path, convert):
     return 0
 
 def download_and_save_video(url, path, convert):
+  print('starting download thread...')
   thread        = threading.Thread(target=_download_and_save_video, args=(url, path, convert))
   thread.daemon = True
   thread.start()
